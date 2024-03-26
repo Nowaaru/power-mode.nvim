b3f24d639a626cf9c54025b23ef44eba35d772b5
Nowaaru <nowaaru@proton.me>
2024-03-26 01:50:24 -0700

    giga-supercharge proxy implementation
    yaya

--- src/lua/power-mode/proxy/init.lua
+++ src/lua/power-mode/proxy/init.lua
@@ -1,8 +1,23 @@
+-- @name Proxy
+-- @rev 2024/03/26
+-- @by Nowaaru
+-- @license gpl3.0
+--
+-- @desc
+-- Makes an interface that takes
+-- metatable preservation into very, very
+-- careful consideration.
+
 ---@class ProxyConstructor
----@overload fun(of: table): Proxy
-local Proxy = setmetatable({
+---@field __VERBOSE string Whether the constructor should automatically log changes.
+---@overload fun(self: ProxyConstructor, of: table, proxyName?: string): Proxy
+local ProxyConstructor = setmetatable({
     __VERBOSE = false,
 }, {
+    ---@param self ProxyConstructor The table responsible for this call.
+    ---@param of table The table that will be turned into a proxy.
+    ---@param proxyName? table The name of the proxy.
+    ---
     ---@return Proxy proxy An interface of "of."
     __call = function(self, of, proxyName)
         local obj = {};
@@ -16,6 +31,7 @@ local Proxy = setmetatable({
         end
 
 
+        ---@nodiscard
         local proxyFunctionHandler = function(metamethod, ...)
             local maybe_func = rawget(ofMt, metamethod);
             if (type(maybe_func) == "function") then
@@ -23,7 +39,6 @@ local Proxy = setmetatable({
             end
         end
 
-        --a
         ---@class Proxy
         -- apply second layer metatable to preserve object mt
         -- also prioritize previous __index before defaulting to
@@ -32,7 +47,21 @@ local Proxy = setmetatable({
         -- over the previous __newindex (but still call it as a side-effect)
         return setmetatable({},
             setmetatable({
-                __index = function(this, k)
+                -- yoinked from LuaUsers
+                __pairs = function()
+                    local function stateless_iter(tbl, k)
+                        local v;
+
+                        k, v = next(tbl, k);
+                        if nil ~= v then
+                            return k, v;
+                        end
+                    end
+
+                    return stateless_iter, obj, nil
+                end,
+
+                __index = function(_, k)
                     local pfhResult = { proxyFunctionHandler("__index", obj, k) };
                     if ((table.maxn and table.maxn(pfhResult) or #pfhResult) == 0) then
                         return rawget(obj, k);
@@ -41,7 +70,7 @@ local Proxy = setmetatable({
                     return unpack(pfhResult);
                 end,
                 __newindex = function(this, k, v)
-                    local onPropertyChangedListener = rawget(obj, "_on" .. k .. "Changed");
+                    local onPropertyChangedListener = rawget(obj, "__" .. k .. "Changed");
                     local typeofListener = type(onPropertyChangedListener);
                     if (onPropertyChangedListener) then
                         if (typeofListener ~= "function") then
@@ -56,11 +85,11 @@ local Proxy = setmetatable({
                                     "%s on proxy %s was changed. (%s -> %s)",
                                     k,
                                     proxyName or ("<anonymous: %s>"):format(tostring(this):gsub("table:", "")),
-                                    rawget(this, k) or "<undefined>",
+                                    rawget(obj, k) or "<undefined>",
                                     type(v) ~= "nil" and v or "<undefined>"
                                 ));
                         end
-                        onPropertyChangedListener(this, rawget(this, k), v);
+                        onPropertyChangedListener(this, rawget(obj, k), v);
                     end
 
 
@@ -79,11 +108,8 @@ local Proxy = setmetatable({
 });
 
 ---@return Proxy proxy An interface of "of".
-Proxy.new = function(of)
-    return getmetatable(Proxy).__call(of);
+ProxyConstructor.new = function(...)
+    return getmetatable(ProxyConstructor).__call(ProxyConstructor, ...);
 end
 
-
-local a = Proxy { a = 4, }
-
-return Proxy;
+return ProxyConstructor;
