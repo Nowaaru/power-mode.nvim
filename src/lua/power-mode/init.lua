91799d07035731787877f22d790f6da144dd2570
Nowaaru <nowaaru@proton.me>
2024-04-14 13:51:47 -0700

    move scorekeeping out of main module

--- src/lua/power-mode/init.lua
+++ src/lua/power-mode/init.lua
@@ -7,6 +7,7 @@ local module      = {};
 local unpack      = table.unpack or unpack;
 local PowerWindow = require("power-mode.power-window");
 local PowerLayer  = require("power-mode.power-layer")
+local Scorekeep   = require("power-mode.scorekeep")
 
 function module:assert(a, msg)
     if (not a) then
@@ -66,20 +67,6 @@ function module:setup()
         win:RenderWindow();
     end
 
-    ---@type table<string, StoreItem>
-    self.test_buffer_store = setmetatable({},
-        {
-            __index = function(this, k)
-                return rawget(this, tostring(k))
-            end,
-            __newindex = function(this, k, v)
-                rawset(this, tostring(k), v);
-            end,
-            __call = function(this)
-                return (this[vim.api.nvim_get_current_buf()] or this:test_make_default_storeitem()).score
-            end
-        });
-
     local ns_id = vim.api.nvim_create_namespace('power-mode');
     local win = PowerWindow.new();
     win:BindToNamespace(ns_id);
@@ -99,118 +86,26 @@ function module:setup()
     -- TODO: Implement score decay type option
     -- (stamina, rapid-decay, no-decay)
 
-    -- TODO: Implement function-based modal multipliers
-    -- to allow users to achieve efficiency goals
-
-    -- TODO: Implement a "flow" multiplier where as long
-    -- as the user makes consistent changes then a
-    -- multiplier is applied to the added score. Length
-    -- does of changes does not matter as much to prevent
-    -- abuse of other modes.
-    --
-    -- Non-insert mode changes count as 2.
-
     -- TODO: Add some kind of consistency indicator bar
-    local timerIntervalMs          = (1 / 60) * 1000;
-    local scoreDecreaseCount       = 3 * timerIntervalMs;
-    local scoreIncrease            = 3;
-    local scoreCap                 = 10;
-
-    local timeBeforeComboRemovalMs = 15 * 1000; -- 15 seconds (testing, maybe permanent as a default?)
-    local flowMaxMultiplier        = 2.87;
-
-    local euler                    = 2.71828
-    local calculateConsistency     = function(amountCharactersTyped, converge)
-        converge = converge or flowMaxMultiplier;
-        return 1 + math.max(1, math.min(converge, 0.8 * math.log(math.pow(amountCharactersTyped, 0.7), euler)));
-    end
-
-    local consistencyDecreaseRate  = 1 / (math.pow(flowMaxMultiplier, 1.3)); -- @ 2.87 ~ 8% loss per tick
-
-    local fixedInterval            = (timerIntervalMs < 1) and timerIntervalMs * 1000 or timerIntervalMs;
-
-    --- WARNING: Modes not specified in this
-    --- table will **not** be whitelisted.
-    local modalMultipliers         = {
-        v = 2.5,
-        n = 2,
-        i = 1
-    };
-
-    self.scoreIncreaseTimer        = vim.loop.new_timer();
-    self.scoreIncreaseTimer:start(0, fixedInterval, function()
-        for buffer, scoreItem in pairs(self.test_buffer_store) do
-            if scoreItem.length_prev == -1 then
-                goto update_length;
-            end
-
-            --[[
-            --do-end required to prevent scope-hopping
-            --]]
-            do
-                --[[
-                    doesn't matter what the changes were! just needs
-                    to be not nothing
-                --]]
-                --TODO: move into anywhere else please god
-                scoreItem.consistency = calculateConsistency(scoreItem.combo);
-                local currentMode = vim.api.nvim_get_mode().mode;
-                local isCurrentModeNormal = currentMode == "n";
-                local lengthDelta = (scoreItem.length_prev - scoreItem.length);
-                local lengthDeltaAbs = math.abs(lengthDelta) * (modalMultipliers[currentMode] or 1)
-                local lengthDeltaModalClamped = isCurrentModeNormal and math.log10(lengthDeltaAbs) or lengthDeltaAbs;
-                local baseAddedScore = math.max(0, math.min(scoreCap,
-                    (scoreIncrease * lengthDeltaModalClamped * scoreItem.consistency) / (timerIntervalMs * 0.5)));
-
-
-                scoreItem.time = scoreItem.time + timerIntervalMs;
-                scoreItem.score = math.min(scoreCap, scoreItem.score + (baseAddedScore));
-
-                if (scoreItem.state_decrease >= scoreDecreaseCount) then
-                    -- if (scoreItem.state_decrease % scoreDecreaseCount == 0) then
-                    scoreItem.score = math.max(scoreItem.score - math.abs((0.5 * scoreIncrease) / timerIntervalMs), 0)
-                    -- end
-                end
-
-                if (scoreItem.time > timeBeforeComboRemovalMs) then
-                    scoreItem.combo = 0;
-                end
-
-                if (scoreItem.length == scoreItem.length_prev) then
-                    scoreItem.state_decrease = scoreItem.state_decrease + 1
-                    scoreItem.consistency = (scoreItem.consistency - consistencyDecreaseRate)
-                else
-                    scoreItem.state_decrease = 0;
-                end
-            end
-
-            ::update_length::
-            local score_maintained = scoreItem.score / scoreCap;
-            scoreItem.length_prev = scoreItem.length;
-            vim.schedule(function()
-                if (buffer == tostring(vim.api.nvim_get_current_buf())) then
-                    bar:Clear();
-                    -- print(scoreItem.score, ":", score_maintained, ("(%i/%s)"):format(score_maintained, scoreCap))
-                    bar:Bar(0, 1, score_maintained, "#FFFFFF" --[[  "#CF3369" ]]);
-                    updateWindow(win, { background, bar });
-                end
-            end)
-        end
-    end)
+    self.scoreIncreaseTimer = vim.loop.new_timer();
 
     -- window moving and working the renderer
+
+
+    local scorekeep = Scorekeep.new(powerModeGroup);
+    scorekeep:Start();
+    ---@param scoreItem ScoreEntry
+    scorekeep.ScoreUpdated = function(scoreItem)
+        bar:Clear();
+        bar:Bar(0, 1, scoreItem.score, "#FFFFFF" --[[  "#CF3369" ]]);
+        updateWindow(win, { background, bar });
+    end
+
     vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
         group = powerModeGroup,
         callback = function(args)
+            scorekeep:Ensure(args.buf);
             updateWindow(win, args)
-        end
-    });
-
-    vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
-        group = powerModeGroup,
-        callback = function(args)
-            self:on_buffer_text_changed(args);
-            win:RenderComponents();
         end
     });
 
@@ -221,53 +116,12 @@ function module:setup()
         end,
 
     });
-end
 
-function module:on_buffer_text_changed(args)
-    local bufferOffset = 2; -- unsure why but the buffer length is always increased by 2 :thonk:
-    local bufferLength = math.max(0, vim.fn.line2byte(vim.fn.line("$") + 1) - bufferOffset);
-
-    local storeItem = self:store_item_or(args.buf);
-    storeItem.length = bufferLength;
-    storeItem.time = 0;           -- time should reset
-    storeItem.state_decrease = 0; -- since they typed, resume combo
-end
-
-function module:store_item_or(bufferId)
-    bufferId = tostring(bufferId);
-    local storeItem = self.test_buffer_store[bufferId];
-    if (not storeItem) then
-        storeItem = self:test_make_default_storeitem();
-        self.test_buffer_store[bufferId] = storeItem;
-    end
-
-    return storeItem;
-end
-
-function module:test_make_default_storeitem()
-    ---@class StoreItem
-    ---@field length_prev integer The length of the buffer during the last pass.
-    ---@field length integer The current length of the buffer.
-    ---@field score number The score belonging to this buffer.
-    ---@field time integer The remaining time before the combo resets.
-    ---@field combo integer How long the user has been consistently typing.
-    ---@field consistency number The consistency multiplier derived from the combo.
-    ---@field state_decrease integer How many passes have occured without a notable change.
-    return {
-        length_prev = -1,
-        length = 0,
-
-        score = 0,
-        time = 0,
-        combo = 0,
-        consistency = 0,
-        state_decrease = 0,
-    };
+    return Scorekeep;
 end
 
 function module:__test(imageNvim)
     local Particle = require("power-mode.particle").WithImages(imageNvim);
-
     local newParticle = Particle.new(nil, nil, 16, "df2935-16x16.png");
     newParticle.X = 25;
     newParticle.Y = 72;
---
+++ src/lua/power-mode/scorekeep.lua
@@ -0,0 +1,182 @@
+---@class ScorekeepConstructor
+local Scorekeep       = {};
+local euler           = 2.71828
+
+---@class ScorekeepPrototype
+---@field _buffers table<string, ScoreEntry>
+---@field _modalMultipliers table<string, number>
+---@field scoreIncreaseTimer unknown
+---@field ScoreUpdated function?
+Scorekeep.__prototype = {
+    --- WARNING: Modes not specified in this
+    --- table will **not** be whitelisted.
+    _modalMultipliers        = {
+        v = 2.5,
+        n = 2,
+        i = 1
+    },
+
+    flowMaxMultiplier        = 2.87,
+    scoreIncreaseTimer       = nil,
+    timerIntervalMs          = (1 / 60) * 1000,
+    scoreDecreaseCount       = 3,
+    timeBeforeComboRemovalMs = 15 * 1000, -- 15 seconds (testing, maybe permanent as a default?)
+    ScoreUpdated = nil;
+    _buffers                 = {},
+
+    ScoreHandler             = setmetatable({}, {
+        __tostring = "Default Score Handler",
+        __call = function(self, scoreItem)
+            -- local self.timerIntervalMs          = (1 / 60) * 1000;
+            -- local
+            -- local self.scoreIncrease            = 3;
+            -- local self.scoreCap                 = 10;
+            --
+            -- local self.
+            -- local self.flowMaxMultiplier        = 2.87;
+            --
+            --
+            -- local self.consistencyDecreaseRate  = 1 / (math.pow(self.flowMaxMultiplier, 1.3)); -- @ 2.87 ~ 8% loss per tick
+
+
+            if scoreItem.length_prev == -1 then
+                goto update_length;
+            end
+            local modalMultipliers = self._modalMultipliers;
+
+            --[[
+            --do-end required to prevent scope-hopping
+            --]]
+            --[[
+                    doesn't matter what the changes were! just needs
+                    to be not nothing
+                --]]
+            --TODO: move into anywhere else please god
+            scoreItem.consistency = self:calculateConsistency(scoreItem.combo);
+            local currentMode = vim.api.nvim_get_mode().mode;
+            local isCurrentModeNormal = currentMode == "n";
+            local lengthDelta = (scoreItem.length_prev - scoreItem.length);
+            local lengthDeltaAbs = math.abs(lengthDelta) * (modalMultipliers[currentMode] or 1)
+            local lengthDeltaModalClamped = isCurrentModeNormal and math.log10(lengthDeltaAbs) or lengthDeltaAbs;
+            local baseAddedScore = math.max(0, math.min(self.scoreCap,
+                (self.scoreIncrease * lengthDeltaModalClamped * scoreItem.consistency) / (self.timerIntervalMs * 0.5)));
+
+
+            scoreItem.time = scoreItem.time + self.timerIntervalMs;
+            scoreItem.score = math.min(self.scoreCap, scoreItem.score + (baseAddedScore));
+
+            if (scoreItem.state_decrease >= self.scoreDecreaseCount) then
+                -- if (scoreItem.state_decrease % scoreDecreaseCount == 0) then
+                scoreItem.score = math.max(scoreItem.score - math.abs((0.5 * self.scoreIncrease) / self.timerIntervalMs),
+                    0)
+                -- end
+            end
+
+            if (scoreItem.time > self.timeBeforeComboRemovalMs) then
+                scoreItem.combo = 0;
+            end
+
+            if (scoreItem.length == scoreItem.length_prev) then
+                scoreItem.state_decrease = scoreItem.state_decrease + 1
+                scoreItem.consistency = (scoreItem.consistency - self.consistencyDecreaseRate)
+            else
+                scoreItem.state_decrease = 0;
+            end
+
+            ::update_length::
+            scoreItem.length_prev = scoreItem.length;
+            if self.ScoreUpdated then
+                vim.schedule(function()
+                    self.ScoreUpdated(scoreItem);
+                end)
+            end
+        end
+    })
+
+};
+
+function Scorekeep:MakeDefault()
+    ---@class ScoreEntry
+    ---@field length_prev integer The length of the buffer during the last pass.
+    ---@field length integer The current length of the buffer.
+    ---@field score number The score belonging to this buffer.
+    ---@field time integer The remaining time before the combo resets.
+    ---@field combo integer How long the user has been consistently typing.
+    ---@field consistency number The consistency multiplier derived from the combo.
+    ---@field state_decrease integer How many passes have occured without a notable change.
+    return {
+        length_prev = -1,
+        length = 0,
+
+        score = 0,
+        time = 0,
+        combo = 0,
+        consistency = 0,
+        state_decrease = 0,
+    };
+end
+
+function Scorekeep.__prototype:Ensure(bufferId)
+    bufferId = tostring(bufferId);
+    local storeItem = self._buffers[bufferId];
+    if (not storeItem) then
+        storeItem = Scorekeep:MakeDefault();
+        self._buffers[bufferId] = storeItem;
+    end
+
+    return storeItem;
+end
+
+function Scorekeep.__prototype:on_buffer_text_changed(args)
+    local managed_buf = self._buffers[tostring(args.buf)];
+    if (not managed_buf) then
+        return;
+    end
+
+    local bufferOffset = 2; -- unsure why but the buffer length is always increased by 2 :thonk:
+    local bufferLength = math.max(0, vim.fn.line2byte(vim.fn.line("$") + 1) - bufferOffset);
+
+
+    local storeItem = self:Ensure(args.buf);
+    storeItem.length = bufferLength;
+    storeItem.time = 0;           -- time should reset
+    storeItem.state_decrease = 0; -- since they typed, resume combo
+end
+
+function Scorekeep.__prototype:Start()
+    local fixedInterval = (self.timerIntervalMs < 1) and self.timerIntervalMs * 1000 or self.timerIntervalMs;
+    self.scoreIncreaseTimer:start(0, fixedInterval, function()
+        for _, scoreItem in pairs(self._buffers) do
+            self:ScoreHandler(scoreItem)
+        end
+    end)
+end
+
+function Scorekeep.__prototype:Stop()
+    self.scoreIncreaseTimer:stop();
+end
+
+function Scorekeep.__prototype:calculateConsistency(amountCharactersTyped, converge)
+    converge = converge or self.flowMaxMultiplier;
+    return 1 + math.max(1, math.min(converge, 0.8 * math.log(math.pow(amountCharactersTyped, 0.7), euler)));
+end
+
+---comment
+---@param powerModeGroup integer
+function Scorekeep.new(powerModeGroup)
+    local obj = setmetatable({}, {
+        __index = Scorekeep.__prototype,
+    })
+
+    vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
+        group = powerModeGroup,
+        callback = function(args)
+            obj:on_buffer_text_changed(args);
+        end
+    });
+
+    obj.scoreIncreaseTimer = vim.loop.new_timer();
+    return obj;
+end
+
+return Scorekeep
